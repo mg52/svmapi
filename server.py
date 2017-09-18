1e4f629dec7f9e9dad57cced743c23e951cd3a9f
Mustafa - Mussky <mgords@gmail.com>
2017-09-18 16:18:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,23 +8,24 @@ from sklearn.svm import SVC
 
 app = Flask(__name__)
 
-MONGO_URL = os.environ.get('MONGODB_URI') 
+MONGO_URL = os.environ.get('MONGODB_URI')
 client = MongoClient(MONGO_URL)
 #client = MongoClient('mongodb://localhost:27017/') #For Locally
 db = client['heroku_mlxjr59b']
-
-#clf = SVC()
 
 def generateApiKeyFunc():
     theKey = hexlify(os.urandom(15)).decode()
     db.keys.insert_one({'apiKey': theKey})
     return theKey
 
+
 def validateApiKey(theKey):
     for doc in db.keys.find({}):
-        if(theKey == doc["apiKey"]):
+        if (theKey == doc["apiKey"]):
             return True
     return False
+
+
 """
 def checkApiKey(inputApiKey):
     for doc in db.keys.find({}):
@@ -41,15 +42,19 @@ def require_appkey(view_function):
             if request.args.get('key') and request.args.get('key') == doc["apiKey"]:
                 return view_function(*args, **kwargs)
         abort(401)
+
     return decorated_function
+
 
 @app.route('/generateApiKey', methods=['GET'])
 def generateApiKey():
     return generateApiKeyFunc()
 
+
 @app.route('/validateApiKey/<inputKey>', methods=['GET'])
 def checkTheKey(inputKey):
     return jsonify(validateApiKey(inputKey))
+
 
 @app.route('/getApiKeys', methods=['GET'])
 def getApiKeys():
@@ -58,6 +63,7 @@ def getApiKeys():
         data['keys'].append(doc["apiKey"])
     return jsonify(data)
 
+
 @app.route('/test', methods=['GET', 'POST'])
 def fun():
     if request.method == 'POST':
@@ -65,40 +71,42 @@ def fun():
     else:
         return jsonify('Get Test OK')
 
+
 @app.route('/train', methods=['POST'])
 @require_appkey
 def trainData():
     str = request.args["x"]
-    input = list(map(int, str.replace('[', '').replace(']', '').split(',')))
-    x = [input[i:i+int(len(input)/(str.count('[') - 1))] for i in range(0, len(input), int(len(input)/(str.count('[') - 1)))]
-    x = np.array(x)
-    
+    input = list(map(float, str.replace('[', '').replace(']', '').split(',')))
+    x = [input[i:i + int(len(input) / (str.count('[') - 1))] for i in range(0, len(input), int(len(input) / (str.count('[') - 1)))]
+
     str2 = request.args["y"]
-    y = list(map(int, str2.replace('[', '').replace(']', '').split(',')))
-    y = np.array(y)
-    
+    y = list(map(float, str2.replace('[', '').replace(']', '').split(',')))
+
     try:
-        db.SVMData.insert_one({"data": {"apiKey": request.args["key"],"x": x,"y": x,"predict": [],"predicted": 0}})
-        #clf.fit(x, y)
+        db.SVMData2.insert_one({"data": {"apiKey": request.args["key"], "x": x, "y": y}})
         return jsonify('Data Trained.')
+
     except:
         return jsonify('ERROR: Data CANNOT Be Trained.')
-    
+
+
 @app.route('/predict', methods=['POST'])
 @require_appkey
 def predictData():
     str = request.args["x"]
-    x = list(map(int, str.replace('[', '').replace(']', '').split(',')))
-    x = np.array(x)
+    input = list(map(float, str.replace('[', '').replace(']', '').split(',')))
+    x = [input[i:i + int(len(input) / (str.count('[') - 1))] for i in range(0, len(input), int(len(input) / (str.count('[') - 1)))]
+    
     try:
-        """data = db.SVMData.find_one({'data.apiKey':request.args["key"]})
-        data["data"]["predict"] = x
-        db.SVMData.update_one({'data.apiKey':request.args["key"]}, {"$set": data}, upsert=False)
-        #predictedData = clf.predict(x)
-        return jsonify('Data Saved.')"""
-        return jsonify('Data Saved.')
+        data = db.SVMData2.find_one({'data.apiKey':request.args["key"]})
+        clf = SVC()
+        clf.fit(np.array(data['data']['x']), np.array(data['data']['y']))
+        predicted = clf.predict(np.array(x))
+        return jsonify(predicted)
     except:
         return jsonify('ERROR: Data CANNOT Be Predicted.')
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=True)
